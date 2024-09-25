fabdbe1d15817b49c9d5d07f5ec33eadc4bda933
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-09-25 15:05:09 +0900

    0.28.0-beta1 릴리즈

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 
 import PackageDescription
 
-let version = "0.27.0"
-let checksum = "4e0843b932aa7a3eabab808d919e0bda9a730315b595a5647a08f3b0572c37a6"
+let version = "0.28.0-beta1"
+let checksum = "9dd1b4a1cecfb16f6e6eda2bf370d58304db3acfccca72feacbba3ffdbce3ed9"
 
 let package = Package(
     name: "AdisonOfferwallGlobalCore",
@@ -12,19 +12,26 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AdisonOfferwallGlobalCore",
-            targets: ["AdisonOfferwallGlobalCore"]
+            targets: ["AdisonOfferwallGlobalCore", "AdisonOfferwallGlobalCoreTarget"]
         ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adison-ads/adison-offerwall-common", exact: "0.9.0-beta1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
             name: "AdisonOfferwallGlobalCore",
-            url: """
-            https://github.com/adison-ads/adison-offerwall-global-core/releases/download/\(version)/\
-            AdisonOfferwallGlobalCore.zip
-            """,
+            url: "https://github.com/adison-ads/adison-offerwall-global-core/releases/download/\(version)/AdisonOfferwallGlobalCore.zip",
             checksum: checksum
+        ),
+        .target(
+            name: "AdisonOfferwallGlobalCoreTarget",
+            dependencies: [
+                .product(name: "AdisonOfferwallCommon", package: "adison-offerwall-common")
+            ],
+            path: "Sources"
         )
     ]
 )
